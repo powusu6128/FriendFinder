1261f0102f238ebaba613cec8d0c8b0928938ade
Philip Owusu <philipowusu@Philips-MacBook-Pro.local>
2018-06-16 04:19:55 -0500

    ignoring some file

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 // app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require("./routing/apiRoutes.js")(app);
+require("./../routing/apiRoutes.js")(app);
 
-require("./routing/htmlRoutes.js")(app);
+require("./../routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
