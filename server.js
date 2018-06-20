75000c16c13bcc479984219ca26baa7d8df1d390
Philip Owusu <philipowusu@er-10-129-122-38.wireless.umn.edu>
2018-06-19 22:07:00 -0500

    redeploying heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require("path");
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("./public"));
+app.use(express.static("./app/public"));
 
 //Make public directroy visible to for the home.html
 app.use(express.static(path.join(__dirname, './app/public')));
@@ -18,9 +18,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 // app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require("./../routing/apiRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
 
-require("./../routing/htmlRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
